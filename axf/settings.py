0836ae1cd93f669e8957c5f0230daed3664f7ae0
Datingzi <1904536691@qq.com>
2018-11-27 20:52:12 +0800

    初步修改

--- axf/settings.py
+++ axf/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '1h9ys%904nd41c5e&4izt#9q6gzlp7#-4a)95gte*h_zgt_aic'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'axfApp',
 ]
 
 MIDDLEWARE = [
@@ -75,7 +76,7 @@ WSGI_APPLICATION = 'axf.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
